bf55b8740ed4e3d33369310990579ac047204522
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2019-02-21 11:22:36 +0600

    Update Version

--- src/Rg.Plugins.Popup.UWP/Properties/AssemblyInfo.cs
+++ src/Rg.Plugins.Popup.UWP/Properties/AssemblyInfo.cs
@@ -18,3 +18,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+
--- src/Rg.Plugins.Popup/Properties/GlobalAssemblyInfo.cs
+++ src/Rg.Plugins.Popup/Properties/GlobalAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct("Rg.Plugins.Popup")]
 [assembly: AssemblyCopyright("Copyright © rotorgames 2015-2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
