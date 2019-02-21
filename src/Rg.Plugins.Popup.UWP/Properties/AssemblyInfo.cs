bdb5cb6e20436f5be4f1a2cd628377b3ee471995
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2019-02-21 11:26:24 +0600

    Fix duplication

--- src/Rg.Plugins.Popup.UWP/Properties/AssemblyInfo.cs
+++ src/Rg.Plugins.Popup.UWP/Properties/AssemblyInfo.cs
@@ -18,6 +18,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+
 
